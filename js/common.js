eae01b92cda34e84fb023d37c1c2eaf78badee0c
aben-bel <abenezer.sbelay@gmail.com>
2020-03-25 00:58:17 +0300

    implement gallery

--- js/common.js
+++ js/common.js
@@ -1,15 +1,11 @@
-let header;
-let topnav;
-window.onload = ()=>{
-    topnav = document.querySelector(".nav-container");
-    header = document.querySelector(".header");
-}
 window.onscroll = () => {
     return scrolled();
 }
 
 
 function scrolled() {
+    topnav = document.querySelector(".nav-container");
+    header = document.querySelector(".header");
     if (window.pageYOffset > header.offsetHeight) {
         topnav.classList.add("sticky");
     }else{
